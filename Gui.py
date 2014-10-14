e7c9f8187e8910a40b2b1fd03de1be2d2b39fd8f
docooler <docooler@njust.edu.cn>
2014-10-14 15:17:06 +0800

    Update and rename wxEdit.py to Gui.py

--- Gui.py
+++ Gui.py
@@ -3,6 +3,7 @@
 # code by elaiyan (Laiyuan yang)
 import wx
 import os
+import testcases
 
 class wxEdit(wx.Frame):
 	"""docstring for wxEdit"""
@@ -17,6 +18,7 @@ class wxEdit(wx.Frame):
 		menuAbout = filemenu.Append(wx.ID_ABOUT, "&About", "code by elaiyan")
 		# filemenu.AppendSeparator()
 		menuExit = filemenu.Append(wx.ID_EXIT,  "E&xit", "Terminate the program")
+		menuSave = filemenu.Append(wx.ID_SAVE, "S&ave", "save file")
 		
 		# Creating the menubar.
 		menuBar = wx.MenuBar()
@@ -27,6 +29,7 @@ class wxEdit(wx.Frame):
 		self.Bind(wx.EVT_MENU, self.OnAbout, menuAbout)
 		self.Bind(wx.EVT_MENU, self.OnExit, menuExit)
 		self.Bind(wx.EVT_MENU, self.OnOpen, menuOpen)
+		self.Bind(wx.EVT_MENU, self.OnSave, menuSave)
 
 		# self.sizer2 = wx.BoxSizer(wx.HORIZONTAL)
   # 		self.buttons = []
@@ -48,7 +51,12 @@ class wxEdit(wx.Frame):
   		self.control = wx.TextCtrl (self.panel, -1, value="please input testcases.xml", size=(980,400), style=wx.TE_MULTILINE)
   		wx.StaticText(self.panel, -1, label="filer condition", pos=(250,400))
   		wx.StaticText(self.panel, -1, label="hardware", pos=(25,425))
-  		self.choice = wx.Choice(self.panel,-1, (0,450))
+  		self.choice = wx.Choice(self.panel,-1, (0,450), size=(120,20))
+  		wx.StaticText(self.panel, -1, label="number", pos=(300,425))
+  		self.numLabel = wx.StaticText(self.panel, -1, label="", pos=(300,425))
+
+  		filterBtn = wx.Button(self.panel, -1, "Filter Start", pos=(600, 425))
+  		self.Bind(wx.EVT_BUTTON, self.OnFilterStart, filterBtn)
   		# self.listview.SetItem()
 
 		self.Show(True)
@@ -61,14 +69,23 @@ class wxEdit(wx.Frame):
 			self.dirname = dlg.GetDirectory()
 			f = open(os.path.join(self.dirname, self.filename), 'r')
 			self.control.SetValue(f.read())
-			parse_xml(f)
 			f.close()
+			self.tcs = testcases.TestCases(os.path.join(self.dirname, self.filename))
+			pdt = self.tcs.getAllProductType()
+			for x in sorted(pdt.keys()):
+				self.choice.Append(x)		
 
-
-
-			for x in xrange(1,10):
-				self.choice.Append(str(x))		
 		dlg.Destroy()
+	def OnSave(self, e):
+		if self.dirname == None:
+			self.dirname = os.getcwd()
+		dlg = wx.FileDialog(self, "please choice save file", self.dirname, "", "*.txt", wx.SAVE)
+		if dlg.ShowModal() == wx.ID_OK:
+			filename = dlg.GetFilename()
+			dirname = dlg.GetDirectory()
+			self.control.SaveFile(os.path.join(dirname, filename))
+			
+		
 
 	def OnAbout(self, e):
 		dlg = wx.MessageDialog(self, "code by elaiyan", "List case from hardware", wx.OK)
@@ -77,6 +94,26 @@ class wxEdit(wx.Frame):
 	
 	def OnExit(self, e):
 		self.Close(True)
+	def OnFilterStart(self, e):
+		index = self.choice.GetSelection()
+		if index == wx.NOT_FOUND:
+			dlg = wx.MessageDialog(self, "code by elaiyan", "please select hardware", wx.OK)
+			dlg.ShowModal()
+			dlg.Destroy()
+		pdt = self.choice.GetString(index)
+		tcs = self.tcs
+		content = tcs.filterByProductType(pdt)
+		# print content
+		display = ""
+		for x in content:
+			x += "\n"
+			display += x.decode('utf8')
+
+		self.control.SetValue(display)
+		num = tcs.getAllProductType()[pdt]
+		disNum = str(num).decode('utf8')
+		self.numLabel.Label(disNum)
+
 
 
 def parse_xml(fd):
